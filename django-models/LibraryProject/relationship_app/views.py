05ef9c44c52597759f904b8361a3c17aa69cbb4b
mbashas <sethmbasha@gmail.com>
2025-07-20 22:47:17 +0300

    Update relationship_app/views.py and compiled Python files

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic.detail import DetailView
+from django.views.generic import ListView
 from django.http import HttpResponse
 from .models import Book, Author, Librarian
 from .models import Library
